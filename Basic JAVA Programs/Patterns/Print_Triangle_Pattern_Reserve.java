53bc177073c0b262ba5ca1bc1c2f76f032d851db
Meru Tiwari <63155420+mr-tiwari-ji@users.noreply.github.com>
2021-09-25 01:18:23 +0530

    Create Print_Triangle_Pattern_Reserve.java

---
+++ Basic JAVA Programs/Patterns/Print_Triangle_Pattern_Reserve.java
@@ -0,0 +1,22 @@
+class Print_Triangle_Pattern_Reserve{
+
+public static void main(String args[])
+        {
+
+        int row=4;
+
+        for(int i=row-1; i>=0; i--)
+        {
+
+        for(int j=0; j<=i; j++)
+        {
+
+        System.out.print("* ");
+        }
+        System.out.println();
+
+        }
+        }
+        }
+
+
